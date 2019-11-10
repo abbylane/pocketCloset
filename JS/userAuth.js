da42c1772ba2df035c4885a12cf2489b4f1d9b50
Anthony Giuliano <agiulian@nd.edu>
2019-11-10 18:10:33 -0500

    login stuff

---
+++ JS/userAuth.js
@@ -0,0 +1,58 @@
+// Constants
+const loginForm = document.querySelector('#login-form');
+const signupForm = document.querySelector('#signup-form');
+
+/*Check if user is logged in 
+and bring them to home page if so*/
+firebase.auth().onAuthStateChanged(function(user){
+  if(user){
+    // User is signed in
+    window.location.href = "index.html";
+    console.log('success');
+  }
+  else{
+    // No user is signed in
+    // Sit and wait for user to sign in
+  }
+});
+
+/* Login user with email and password */
+if(loginForm){
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+  
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+      // Handle errors here
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      window.alert("Error : " + errorMessage);
+    });
+  });
+}
+
+
+/* Signup user with email, password, and name */
+if(signupForm){
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    const name = signupForm['signup-name'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      db.collection('users').doc(cred.user.uid).set({
+        name: name,
+        email: email,
+        uid: cred.user.uid
+      });
+    });
+  });
+}
