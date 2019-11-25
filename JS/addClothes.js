55e5e1433e01a76d48cc7ef1e6486b7685069a14
alane3 <alane3@nd.edu>
2019-11-25 16:09:21 -0500

    clothing name connected

--- JS/addClothes.js
+++ JS/addClothes.js
@@ -4,15 +4,26 @@ const typeSelect = document.querySelector('#typeSelect');
 const colorSelect = document.querySelector('#colorSelect');
 const notesTextArea = document.querySelector('#notesTextArea');
 const addClothingItemBtn = document.querySelector('#addClothingItemBtn');
+const clothingName = document.querySelector('#newClothingName'); 
+
+// TODO: firebase storage for image uploading  
+var storageRef = firebase.storage().ref();  // root reference
+var imageRef = storageRef.child('test.jpg');  // reference to uploaded image 
+
+let fileName = ""
+
+var tempImagesRef = storageRef.child(`images/${fileName}`);
+
+imageRef.name == tempImagesRef.name; 
+imageRef.fullPath == tempImagesRef.fullPath; 
+
 
 // Add clothing item to database on submit
 addClothingItemBtn.onclick = function(){
   var user = firebase.auth().currentUser;
 
-  // Todo: change this to name provided by user
-  var itemName = colorSelect.options[colorSelect.selectedIndex].value + typeSelect.options[typeSelect.selectedIndex].value;
-
-  usersRef.doc(user.uid).collection('closet').doc(itemName).set({
+  usersRef.doc(user.uid).collection('closet').doc(clothingName.value).set({
+    name: clothingName.value, 
     type: typeSelect.options[typeSelect.selectedIndex].value,
     color: colorSelect.options[colorSelect.selectedIndex].value,
     notes: notesTextArea.value
