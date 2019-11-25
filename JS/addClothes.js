68a68ab862997b7d9407221001a205dc36fb76ea
Anthony Giuliano <agiulian@nd.edu>
2019-11-25 18:29:49 -0500

    add clothes with realtime updates

--- JS/addClothes.js
+++ JS/addClothes.js
@@ -1,21 +1,42 @@
 // Constants
 const usersRef = db.collection('users');
+const clothingNameInput = document.querySelector('#clothingNameInput');
 const typeSelect = document.querySelector('#typeSelect');
 const colorSelect = document.querySelector('#colorSelect');
 const notesTextArea = document.querySelector('#notesTextArea');
+const clothingPhotoInput = document.querySelector('#clothingPhotoInput');
 const addClothingItemBtn = document.querySelector('#addClothingItemBtn');
 
 // Add clothing item to database on submit
 addClothingItemBtn.onclick = function(){
   var user = firebase.auth().currentUser;
 
-  // Todo: change this to name provided by user
-  var itemName = colorSelect.options[colorSelect.selectedIndex].value + typeSelect.options[typeSelect.selectedIndex].value;
+  var itemName = clothingNameInput.value;
 
-  usersRef.doc(user.uid).collection('closet').doc(itemName).set({
-    type: typeSelect.options[typeSelect.selectedIndex].value,
-    color: colorSelect.options[colorSelect.selectedIndex].value,
-    notes: notesTextArea.value
-  }, { merge: true });
+  var file = clothingPhotoInput.files[0];
+  var fileName = new Date() + '-' + file.name;
+  var metadata = {contentType: file.type};
+  var uploadTask = firebase.storage().ref().child('images/'+fileName).put(file, metadata);
+
+  uploadTask.on('state_changed', function (snapshot) {
+    var progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+    console.log('upload is ' + progress + ' done');
+  }, function (error) {
+      console.log(error.message);
+  }, function () {
+      uploadTask.snapshot.ref.getDownloadURL().then(function (url) {
+        usersRef.doc(user.uid).collection('closet').doc(itemName).set({
+          name: itemName,
+          type: typeSelect.options[typeSelect.selectedIndex].value,
+          color: colorSelect.options[colorSelect.selectedIndex].value,
+          notes: notesTextArea.value,
+          imageUrl: url
+        }, { merge: true });
+      });
+  }
+  
+  );
+
+  
 }
 
