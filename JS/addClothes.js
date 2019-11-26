1021eb63b2470b92863e62c81c5836c97879d828
Anthony Giuliano <agiulian@nd.edu>
2019-11-25 19:00:49 -0500

    clothing filters

--- JS/addClothes.js
+++ JS/addClothes.js
@@ -8,18 +8,6 @@ const clothingPhotoInput = document.querySelector('#clothingPhotoInput');
 const addClothingItemBtn = document.querySelector('#addClothingItemBtn');
 const clothingName = document.querySelector('#newClothingName'); 
 
-// TODO: firebase storage for image uploading  
-var storageRef = firebase.storage().ref();  // root reference
-var imageRef = storageRef.child('test.jpg');  // reference to uploaded image 
-
-let fileName = ""
-
-var tempImagesRef = storageRef.child(`images/${fileName}`);
-
-imageRef.name == tempImagesRef.name; 
-imageRef.fullPath == tempImagesRef.fullPath; 
-
-
 // Add clothing item to database on submit
 addClothingItemBtn.onclick = function(){
   var user = firebase.auth().currentUser;
@@ -48,57 +36,5 @@ addClothingItemBtn.onclick = function(){
       });
     } 
   );
-}
-
-function showContent(type){
-  var user = firebase.auth().currentUser;
-  console.log(user);
-  console.log(db.collection("closet"));
-  cards = document.getElementById("cardColumns");
-  while (cards.firstChild) {
-    cards.removeChild(cards.firstChild);
-  }
-  db.collection('users').doc(user.uid).collection('closet').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-      console.log(doc.data());
-      if(doc.data().type=='jeans' && type =='pants'){
-        var temp = document.getElementsByTagName("template")[0];
-        var clon = temp.content.cloneNode(true);
-        clon.getElementById("clotheName").textContent = doc.data().name;
-        clon.getElementById("pic").src = "images/blackPants.png";
-        document.getElementById("cardColumns").appendChild(clon);
-
-      }
-
-      if((doc.data().type=='longsleeve' || doc.data().type=='tshirt' || doc.data().type=='sweatshirt' || doc.data().type=='dress') && type =='shirts'){
-        var temp = document.getElementsByTagName("template")[0];
-        var clon = temp.content.cloneNode(true);
-        clon.getElementById("clotheName").textContent = doc.data().name;
-        clon.getElementById("pic").src = "images/blueTShirt.png";
-        document.getElementById("cardColumns").appendChild(clon);
-      }
-
-      if((doc.data().type=='tennisShoes' || doc.data().type=='heels') && type =='shoes'){
-        var temp = document.getElementsByTagName("template")[0];
-        var clon = temp.content.cloneNode(true);
-        clon.getElementById("clotheName").textContent = doc.data().name;
-        clon.getElementById("pic").src = "images/blackShoes.jpeg";
-        document.getElementById("cardColumns").appendChild(clon);     
-      }
-    })
-    
-  });
-  
-  /*
-  document.getElementById("cardColumns").appendChild(clon);
-  for (i = 0; i < myArr.length; i++) {
-    //Create a new node, based on the template:
-    a = document.importNode(item, true);
-    //Add data from the array:
-    a.textContent += myArr[i];
-    //append the new node wherever you like:
-    document.body.appendChild(a);
-  }
-  */
 }
 
